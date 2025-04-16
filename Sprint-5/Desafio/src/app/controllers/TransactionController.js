de0d886e71e97c17f042c9052483acf3f4ab7d4d
matheusaguiarrr <matheustitanio5@gmail.com>
2025-04-16 15:11:04 -0300

    refactor(TransactionController): add try catch, validação de existência do usuário e melhora as respostas e mensagens de erro

--- Sprint-5/Desafio/src/app/controllers/TransactionController.js
+++ Sprint-5/Desafio/src/app/controllers/TransactionController.js
@@ -4,45 +4,57 @@ import * as Yup from 'yup';
 import Account from '../models/Account.js';
 import Institution from '../models/Institution.js';
 import Transaction from '../models/Transaction.js';
+import User from '../models/User.js';
 
 class TransactionController {
 	async index(req, res) {
-		const { id } = req.params;
-		const { institution } = req.query;
-		let dataInstitution = null;
-		if (institution !== undefined) {
-			const normalizedInstitution = normalizeText(institution);
-			dataInstitution = await Institution.findOne({
-				where: { normalized_name: normalizedInstitution },
-			});
-			if (dataInstitution === null) {
-				return res.status(404).json({ error: 'Instituição não encontrada' });
+		try {
+			const { id } = req.params;
+			const { institution } = req.query;
+			let dataInstitution = null;
+			const user = await User.findByPk(id);
+			if (user === null) {
+				return res.status(404).json({ error: 'Usuário não encontrado' });
 			}
-		}
-		let transactions = null;
-		if (!institution) {
-			transactions = await Transaction.findAll({
-				include: {
-					model: Account,
-					as: 'account',
-					where: { user_id: id },
-				},
+			if (institution !== undefined) {
+				const normalizedInstitution = normalizeText(institution);
+				dataInstitution = await Institution.findOne({
+					where: { normalized_name: normalizedInstitution },
+				});
+				if (dataInstitution === null) {
+					return res.status(404).json({ error: 'Instituição não encontrada' });
+				}
+			}
+			let transactions = null;
+			if (!institution) {
+				transactions = await Transaction.findAll({
+					include: {
+						model: Account,
+						as: 'account',
+						where: { user_id: id },
+					},
+				});
+			} else {
+				transactions = await Transaction.findAll({
+					include: {
+						model: Account,
+						as: 'account',
+						where: { user_id: id, institution_id: dataInstitution.id },
+					},
+				});
+			}
+			if (transactions.length === 0) {
+				return res
+					.status(404)
+					.json({ error: 'Não há transações para o usuário informado' });
+			}
+			return res.json({ transactions });
+		} catch (error) {
+			return res.status(500).json({
+				error: 'Erro interno do servidor',
+				details: error.message,
 			});
-		} else {
-			transactions = await Transaction.findAll({
-				include: {
-					model: Account,
-					as: 'account',
-					where: { user_id: id, institution_id: dataInstitution.id },
-				},
-			});
 		}
-		if (transactions.length === 0) {
-			return res
-				.status(404)
-				.json({ error: 'Não há transações para o usuário informado' });
-		}
-		return res.json({ transactions });
 	}
 
 	async store(req, res) {
@@ -55,7 +67,10 @@ class TransactionController {
 					.min(0.01, 'O valor não pode ser negativo')
 					.required('O campo valor é obrigatório'),
 				type: Yup.string()
-					.oneOf(['credit', 'debit'], 'Tipo de transação inválido')
+					.oneOf(
+						['credit', 'debit'],
+						'Tipo inválido. As opções válidas são credit ou debit.',
+					)
 					.required('O campo tipo é obrigatório'),
 			});
 			await schema.validate(req.body, { abortEarly: false, strict: true });
@@ -75,7 +90,7 @@ class TransactionController {
 			if (type === 'debit' && account.balance < value) {
 				return res
 					.status(422)
-					.json({ error: 'Valor maior que o saldo disponível' });
+					.json({ error: 'Saldo insuficiente para realizar a transação' });
 			}
 			const transaction = await Transaction.create({
 				account_id: account.id,
@@ -89,7 +104,7 @@ class TransactionController {
 					? parseFloat((parsedBalance + parsedValue).toFixed(2))
 					: parseFloat((parsedBalance - parsedValue).toFixed(2));
 			await account.update({ balance: newBalance });
-			return res.json({
+			return res.status(201).json({
 				message: 'Transação realizada com sucesso',
 				transaction,
 				new_balance: newBalance,
